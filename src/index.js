fb7bfaec360313149959211f4fa901d5c6752364
yooopi291 <yooopi291@gmail.com>
2026-01-16 13:58:08 +0300

    Update to Node.js 24: fix Vercel deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
